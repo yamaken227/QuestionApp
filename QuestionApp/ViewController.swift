7c2af6bd52eec52b70dd41cd62f0f3547df9b8a7
yamaken227 <74579887+yamaken227@users.noreply.github.com>
2021-04-08 14:55:47 +0900

    Initial Commit

---
+++ QuestionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuestionApp
+//
+//  Created by Kenta Yamada on 2021/04/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
